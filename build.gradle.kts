e7396fcf17f48008cb77b10f759145377f96b559
Stephen Kirk <stephen.kirk@toasttab.com>
2024-10-03 14:34:47 +0100

    Update compose to 1.6.11 (#37)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.20"
-    id("org.jetbrains.compose") version "1.6.0"
+    id("org.jetbrains.compose") version "1.6.11"
     id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
 }
 

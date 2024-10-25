f4f6160dfe101ca0b93545c1946050a81db91cfc
Stephen Kirk <stephen.kirk@toasttab.com>
2024-10-25 16:05:54 +0100

    Fix linting

--- src/main/kotlin/com/toasttab/pulseman/entities/CompileResult.kt
+++ src/main/kotlin/com/toasttab/pulseman/entities/CompileResult.kt
@@ -2,7 +2,6 @@ package com.toasttab.pulseman.entities
 
 import com.toasttab.pulseman.jars.JarLoader
 import com.toasttab.pulseman.pulsar.handlers.PulsarMessageClassInfo
-import javax.script.ScriptEngine
 
 class CompileResult(
     val code: String,
